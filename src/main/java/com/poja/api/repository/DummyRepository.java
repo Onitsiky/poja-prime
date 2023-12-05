f3f67d94b208e0a3d3b28c4cac4b82605d5dc68e
Onitsiky <hei.onitsiky@gmail.com>
2023-12-05 21:54:41 +0300

    feat: initialize poja-prime

---
+++ src/main/java/com/poja/api/repository/DummyRepository.java
@@ -0,0 +1,15 @@
+package com.poja.api.repository;
+
+import com.poja.api.PojaGenerated;
+import com.poja.api.repository.model.Dummy;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@PojaGenerated
+@Repository
+public interface DummyRepository extends JpaRepository<Dummy, String> {
+
+  @Override
+  List<Dummy> findAll();
+}
---
+++ src/main/java/com/poja/api/repository/model/Dummy.java
@@ -0,0 +1,15 @@
+package com.poja.api.repository.model;
+
+import com.poja.api.PojaGenerated;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@PojaGenerated
+@Entity
+@Getter
+@Setter
+public class Dummy {
+  @Id private String id;
+}
